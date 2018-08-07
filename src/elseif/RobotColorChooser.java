19d2face5228cb32b4fe30891d641c62cbefaf33
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-07 14:21:21 -0700

    robot shiny

--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -13,20 +13,36 @@ public class RobotColorChooser {
 	public static void main(String[] args) {
 		//1. Create a new Robot
 
-	static Robot robot = new Robot();
+      Robot  robot= new Robot();
 	System.out.println(robot);
-		//3. Ask the user what color they would like the robot to draw
+	
+	//3. Ask the user what color they would like the robot to draw
+	for (int g = 0; g < 6; g++) {
 		
-		//5. Use an if/else statement to set the pen color that the user requested
-
+	
+	String Color=JOptionPane.showInputDialog("what type color do you want");
+	
+	//5. Use an if/else statement to set the pen color that the user requested
+if(Color.equals("black")) {
+	robot.setPenColor(56,46,23);
+}
+	if (Color.equals("red")){
+			robot.setPenColor(70,5,5);}
         //6. If the user doesn’t enter anything, choose a random color
-
+else if(Color.equals("")) {
+	robot.setPenColor(58,79,20); 
+}
         //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
+
 		//4. Set the pen width to 10
-		
+	
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-robot.move(10);
-robot
+for(int i=0; i<4;i++) {
+	robot.penDown();
+	robot.setSpeed(20);
+	robot.setPenWidth(10);
+	robot.move(100);
+	robot.turn(360/4);
+}}
 	}
 }
--- src/loops/ShinyObjects.java
+++ src/loops/ShinyObjects.java
@@ -8,14 +8,15 @@ import java.applet.AudioClip;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 
 public class ShinyObjects {
 	public static void main(String[] args) {
 		// 2. Ask the user how many shiny objects they want
-
+String input =JOptionPane.showInputDialog("how many shiny objects do you wants");
 		// 3. Play the sound that many times
-
+if 
 		// 1. Call the method below
 
 	}

e294e9e0c7460d359006f34b86c097c7046dc859
38558 <38558@0338S-S-R0B6ZHP.sdcs.local>
2018-09-25 14:29:15 -0700

    walk of fame fizz buzz

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -8,25 +8,29 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
- 	Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+		Robot rob = new Robot();
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
 
- 	rob.setX(100);
- 	rob.setAngle(a);
- 	rob.penDown();
- for (int i = 0; i <5; i++) {
-	 rob.move(100);
-	 rob.turn(144);
-}	
-		// 2. Make the robot draw a star shape. Hint: 144.
+		
+		rob.penDown();
+		for (int i = 0; i < 15; i++) {
+			rob.setX(i*50);
+			rob.setY(i*50);
+			rob.setSpeed(100);
+			rob.setRandomPenColor();
+			for (int j = 0; j < 5; j++) {
+				rob.move(30);
+				rob.turn(144);
+			}
+
+		} // 2. Make the robot draw a star shape. Hint: 144.
 
 		// 3. Set the size of the star to 30.
 
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/walk-of-fame
+		 * Make the robot draw a line of stars like this: http://bit.ly/walk-of-fame
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
--- src/modulo/FizzBuzz.java
+++ src/modulo/FizzBuzz.java
@@ -26,6 +26,12 @@ package modulo;
 **/
 
 public class FizzBuzz {
+	public static void main(String[] args) {
+		
+for(int i=0; i<=20; i++) {
+	if (i%3==0) {
+	System.out.println(i);
 
 }
-
+	}
+}
